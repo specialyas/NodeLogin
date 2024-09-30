e16339589b06f9541d4adaf8e3fa67c4fb90c482
Y A S <54225481+specialyas@users.noreply.github.com>
2024-09-30 12:17:08 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+
+const PORT = process.env.PORT || 4000;
+
+
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req, res) => {
+    res.render("index");
+})
+
+app.get('/users/register', (req, res) => {
+    res.render("register");
+})
+
+app.get('/users/login', (req, res) => {
+    res.render("login");
+})
+
+
+app.get('/users/dashboard/', (req, res) => {
+    res.render("dashboard", {user: "Yusuf"});
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`);
+    
+})
